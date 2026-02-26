f581d0a3f7053da6017d8adc154396c59def75c7
moraakshaya <moraakshayareddy@gmail.com>
2026-02-26 12:10:31 +0530

    updated google analytics code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 // REMOVED /src from the paths below
 import Navbar from '@/components/navbar/Navbar'; 
 import Footer from '@/components/footer/Footer'; 
@@ -9,18 +10,32 @@ export const metadata = {
   icons: {
     icon: '/favicon.png',   // 👈 use app/favicon.png as favicon
   },
+  verification: {
+    google: '59bJe5b02qLO_qXKfZQ5AfHFuuC-VvzRo3DGIZ8zcFg',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* head content moved to app/head.jsx so that metadata API can
-          merge page-specific values. */}
-
       <body>
         <Navbar />
         <main>{children}</main>
         <Footer />
+         {/* ✅ Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9WBPT4T1YW"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9WBPT4T1YW');
+          `}
+        </Script>
       </body>
     </html>
   );
